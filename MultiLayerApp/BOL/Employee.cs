61d4f480671abad80a58313c7a030de4a69922e5
Vaishnav Puram <puramv307@gmail.com>
2024-01-09 23:02:46 +0530

    added Business Object Layer

---
+++ MultiLayerApp/BOL/Employee.cs
@@ -0,0 +1,41 @@
+namespace BOL;
+[Serializable]
+public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int NoOfHrsWorked { get; set; }
+    public double DA { get; set; }
+    public double BasicSal { get; set; }
+    public int Tax { get; set; }
+    public string Dept { get; set; }
+
+    public EmpType EType { get; set; }
+    public DateTime JoiningDate { get; set; }
+
+    public Employee(){}
+
+    public Employee(int Id, string Name, int NoOfHrsWorked, double BasicSal, int Tax, string Dept, double DA, EmpType EType, DateTime JoiningDate)
+    {
+        this.Id = Id;
+        this.Name = Name;
+        this.NoOfHrsWorked = NoOfHrsWorked;
+        this.BasicSal = BasicSal;
+        this.Tax = Tax;
+        this.Dept = Dept;
+        this.DA = DA;
+        this.EType = EType;
+        this.JoiningDate = JoiningDate;
+    }
+    public double computeSal()
+    {
+        double finalSal;
+        int T = this.Tax / 100;
+        finalSal = this.BasicSal + (this.NoOfHrsWorked * this.DA) - T;
+        return finalSal;
+    }
+    public override string ToString()
+    {
+        return this.Id + " " + this.Name + " " + this.EType + " " + this.NoOfHrsWorked + " " + this.BasicSal + " " + this.JoiningDate + " " + this.Tax + " " + this.Dept;
+    }
+}
