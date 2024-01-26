2c29976c45068ee8a12052ed9d0e1ff509486bd7
Vaishnav Puram <puramv307@gmail.com>
2024-01-26 18:01:08 +0530

    Partial Classes

---
+++ C#Basics/C#Basics/PartialDemo/PartialEmp1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Basics.PartialDemo
+{
+    public partial class PartialEmp
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public double Salary { get; set; }
+    }
+}
---
+++ C#Basics/C#Basics/PartialDemo/PartialEmp2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Basics.PartialDemo
+{
+    public partial class PartialEmp
+    {
+        public void DisplayFullName()
+        {
+            Console.WriteLine($"Full Name is : {FirstName} {LastName}");
+        }
+        public void DisplayEmployeeDetails()
+        {
+            Console.WriteLine("Employee Details : ");
+            Console.WriteLine($"First Name : {FirstName}");
+            Console.WriteLine($"Last Name : {LastName}");
+            Console.WriteLine($"Gender : {Gender}");
+            Console.WriteLine($"Salary : {Salary}");
+        }
+    }
+}
