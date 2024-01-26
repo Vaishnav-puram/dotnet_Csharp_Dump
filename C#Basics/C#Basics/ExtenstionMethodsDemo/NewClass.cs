b48839e815a18d097a4557094409c533bbbbc294
Vaishnav Puram <puramv307@gmail.com>
2024-01-26 18:00:27 +0530

    Extenstion methods

---
+++ C#Basics/C#Basics/ExtenstionMethodsDemo/NewClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Basics.ExtenstionMethodsDemo
+{
+    public static class NewClass
+    {
+        public static void Test3(this OldClass O)
+        {
+            Console.WriteLine("Method Three");
+        }
+        public static void Test4(this OldClass O, int x)
+        {
+            Console.WriteLine("Method Four: " + x);
+        }
+        public static void Test5(this OldClass O)
+        {
+            Console.WriteLine("Method Five:" + O.x);
+        }
+    }
+}
---
+++ C#Basics/C#Basics/ExtenstionMethodsDemo/OldClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Basics.ExtenstionMethodsDemo
+{
+    public class OldClass
+    {
+        public int x = 100;
+        public void Test1()
+        {
+            Console.WriteLine("Method one: " + this.x);
+        }
+        public void Test2()
+        {
+            Console.WriteLine("Method two: " + this.x);
+        }
+    }
+}
